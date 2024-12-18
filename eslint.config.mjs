7f65919ed1a6eb750f3983d65613f7450d79c467
runardvl <runarshakirov@gmail.com>
2024-12-18 18:45:40 +0300

    feat(core): создает weather sound app

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,12 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
 ];

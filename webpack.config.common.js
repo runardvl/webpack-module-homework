977e313b9fe935c7b7ffc855b27d0e48d500f551
runardvl <runarshakirov@gmail.com>
2024-12-20 05:53:44 +0300

    chor(config): переносит статику в dist
    Переносит изображения разных форматов, svg, mp3 в dist

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -58,6 +58,31 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/icons/[name][ext]",
+        },
+      },
+      {
+        test: /\.mp3$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/sounds/[name][ext]",
+        },
+      },
     ],
   },
 };
